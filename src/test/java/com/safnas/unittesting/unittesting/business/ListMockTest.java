3a883443f4fbd908e0890132a7b55d28f6a12005
Safnas <iamsafnas@gmail.com>
2020-12-08 23:34:08 +0530

    verify min/max number of invocations

--- src/test/java/com/safnas/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/safnas/unittesting/unittesting/business/ListMockTest.java
@@ -6,12 +6,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ListMockTest {
 
-    List mock = mock(List.class);
+    List<String> mock = mock(List.class);
 
     @Test
     public void size_basic(){
@@ -39,5 +38,17 @@ public class ListMockTest {
         when(mock.get(anyInt())).thenReturn("Safnas");
         assertEquals("Safnas", mock.get(0));
         assertEquals("Safnas", mock.get(1));
+    }
+
+    @Test
+    public void verificationBasics(){
+        //SUT
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+
+        //verify
+        verify(mock).get(0);
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atLeast(1)).get(anyInt());
     }
 }
