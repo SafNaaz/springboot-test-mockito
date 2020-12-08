1e29dd2530be48e61a9f6113686b7efbca167e14
Safnas <iamsafnas@gmail.com>
2020-12-08 22:37:18 +0530

    using annotations

--- src/test/java/com/safnas/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/safnas/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -1,41 +1,44 @@
 package com.safnas.unittesting.unittesting.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.safnas.unittesting.unittesting.data.SomeDataService;
 
-class SomeBusinessMockTest {
-	
-	SomeBusinessImpl business = new SomeBusinessImpl();
-	
-	SomeDataService dataServiceMock = mock(SomeDataService.class);
-	
+public class SomeBusinessMockTest {
+
+	@InjectMocks
+	SomeBusinessImpl business;
+
+	@Mock
+	SomeDataService dataServiceMock;
+
 	@BeforeEach
-	public void before() {
-		business.setSomeDataService(dataServiceMock);
+	void beforeEach() {
+		MockitoAnnotations.openMocks(this);
 	}
 
 	@Test
-	void calculateSumUsingDataService_basic() {
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
+	public void calculateSumUsingDataService_basic() {
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2, 3 });
 		assertEquals(6, business.calculateSumUsingDataService());
 	}
-	
+
 	@Test
-	void calculateSum_empty() {
+	public void calculateSumUsingDataService_empty() {
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
 		assertEquals(0, business.calculateSumUsingDataService());
 	}
-	
+
 	@Test
-	void calculateSum_oneValue() {
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {5});
+	public void calculateSumUsingDataService_oneValue() {
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 5 });
 		assertEquals(5, business.calculateSumUsingDataService());
 	}
-
 }
