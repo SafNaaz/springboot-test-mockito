59750689fba56e6b0b17b4dd665930bf18966f8e
Safnas <iamsafnas@gmail.com>
2020-12-08 13:36:42 +0530

    testing using a stub when using a dataservice class

---
+++ src/test/java/com/safnas/unittesting/unittesting/business/SomeBusinessStubTest.java
@@ -0,0 +1,43 @@
+package com.safnas.unittesting.unittesting.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.safnas.unittesting.unittesting.data.SomeDataService;
+
+class SomeDataServiceStub implements SomeDataService {
+	@Override
+	public int[] retrieveAllData() {
+		return new int[] {1,2,3};
+	}
+}
+
+class SomeBusinessStubTest {
+
+	@Test
+	void calculateSumUsingDataService_basic() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		business.setSomeDataService(new SomeDataServiceStub());
+		int actualResult = business.calculateSumUsingDataService();
+		int expectedResult = 6;
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void calculateSum_empty() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int [] {});
+		int expectedResult = 0;
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	void calculateSum_oneValue() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int [] {5});
+		int expectedResult = 5;
+		assertEquals(expectedResult, actualResult);
+	}
+
+}
