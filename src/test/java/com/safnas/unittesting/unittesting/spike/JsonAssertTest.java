1f3ee22515d8c2a5f55fe27a62b74a82883eb921
Safnas <iamsafnas@gmail.com>
2020-12-09 20:10:12 +0530

    no escape chars

--- src/test/java/com/safnas/unittesting/unittesting/spike/JsonAssertTest.java
+++ src/test/java/com/safnas/unittesting/unittesting/spike/JsonAssertTest.java
@@ -6,7 +6,7 @@ import org.skyscreamer.jsonassert.JSONAssert;
 
 public class JsonAssertTest {
 
-    String actualResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":11,\"quantity\":100}";
+    String actualResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
 
     @Test
     public void jsonAssert_StrictTrue_ExactMatchExceptForSpaces() throws JSONException {
@@ -16,7 +16,7 @@ public class JsonAssertTest {
 
     @Test
     public void jsonAssert_StrictFalse() throws JSONException {
-        String expectedResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10}";
+        String expectedResponse = "{id:1,name:Ball,price:10}";
         JSONAssert.assertEquals(expectedResponse, actualResponse, false);
     }
 }
