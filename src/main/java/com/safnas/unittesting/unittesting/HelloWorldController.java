499541c1607f4334f1ebc8756492bc21f6e20b4f
Safnas <iamsafnas@gmail.com>
2020-12-09 13:53:20 +0530

    Simple hello world controller and a service

---
+++ src/main/java/com/safnas/unittesting/unittesting/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.safnas.unittesting.unittesting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWOrld(){
+        return "Hello World";
+    }
+}
