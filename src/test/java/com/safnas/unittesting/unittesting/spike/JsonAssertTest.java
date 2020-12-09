3e71ff57ab98bee1539565f66f46e100c2fbfe50
Safnas <iamsafnas@gmail.com>
2020-12-09 20:07:29 +0530

    JSON assert with strick true

---
+++ src/test/java/com/safnas/unittesting/unittesting/spike/JsonAssertTest.java
@@ -0,0 +1,16 @@
+package com.safnas.unittesting.unittesting.spike;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+public class JsonAssertTest {
+
+    String actualResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+
+    @Test
+    public void jsonAssert_StrictTrue_ExactMatchExceptForSpaces() throws JSONException {
+        String expectedResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+        JSONAssert.assertEquals(expectedResponse, actualResponse, true);
+    }
+}
