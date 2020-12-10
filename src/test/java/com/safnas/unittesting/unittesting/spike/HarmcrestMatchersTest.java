d04fd90cfc42c7b077395dd19cf77f5cdae51e0e
Safnas <iamsafnas@gmail.com>
2020-12-10 23:08:46 +0530

    readable tests with harmcrest

---
+++ src/test/java/com/safnas/unittesting/unittesting/spike/HarmcrestMatchersTest.java
@@ -0,0 +1,24 @@
+package com.safnas.unittesting.unittesting.spike;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HarmcrestMatchersTest {
+
+    @Test
+    public void learning(){
+        List<Integer> numbers = Arrays.asList(12,15,45);
+
+        assertThat(numbers, hasSize(3));
+        assertThat(numbers, hasItems(12,45));
+        assertThat(numbers, everyItem(greaterThan(10)));
+
+        assertThat("ABCDE", containsString("BCD"));
+        assertThat("ABCDE", startsWith("ABC"));
+    }
+}
