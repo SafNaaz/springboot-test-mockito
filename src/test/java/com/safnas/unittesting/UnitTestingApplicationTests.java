037e6801d143020f6a274a49895e33077e9f4932
Safnas <iamsafnas@gmail.com>
2020-12-10 22:35:06 +0530

    if specific ppty file is needed

--- src/test/java/com/safnas/unittesting/UnitTestingApplicationTests.java
+++ src/test/java/com/safnas/unittesting/UnitTestingApplicationTests.java
@@ -2,8 +2,10 @@ package com.safnas.unittesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+//@TestPropertySource(locations = {"classpath:test-configuration.properties"}) // if we want specific ppty file
 class UnitTestingApplicationTests {
 
 	@Test
